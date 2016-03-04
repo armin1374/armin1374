597694515d0a114976f3f65739c4a1dc4887f2c4
mrbot001 <mrbot@chmail.ir>
2016-03-04 08:05:23 -0800

    Create kickme.lua

---
+++ plugins/kickme.lua
@@ -0,0 +1,37 @@
+do
+local function kick_user(user_id, chat_id)
+  local chat = 'channel#id'..chat_id
+  local user = 'user#id'..user_id
+  channel_kick_user(chat, user, function (data, success, result)
+    if success ~= 1 then
+      send_msg(data.chat, 'Error while kicking user', ok_cb, nil)
+    end
+  end, {chat=chat, user=user})
+end
+
+local function run (msg, matches)
+  local user = msg.from.id
+  local chat = msg.to.id
+
+  if msg.to.type ~= 'channel' then
+    return "Not a chat group!"
+  elseif user == tostring(our_id) then
+    --[[ A robot must protect its own existence as long as such protection does
+    not conflict with the First or Second Laws. ]]--
+    return "I won't kick myself!"
+ -- elseif is_sudo(msg) then
+   -- return "I won't kick an admin!"
+  else
+    kick_user(user, chat)
+  end
+end
+return {
+  description = "Bot kicks user",
+  usage = {
+    "!kickme"
+  },
+  patterns = {
+    "^[!/#]kickme$"
+  },
+  run = run
+}
